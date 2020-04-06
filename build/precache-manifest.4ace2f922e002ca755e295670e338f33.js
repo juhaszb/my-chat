d9f0d2bdbb6555dbce4a8bd1a160623552b289f1
Juhasz Balazs <juhaszb.98.01@gmail.com>
2020-04-06 18:22:00 +0200

    build enabled

---
+++ build/precache-manifest.4ace2f922e002ca755e295670e338f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8117fa9d2b7a9e353f83b421bb2c28ea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c12c014193c5cd6e99a9",
+    "url": "./static/css/main.93b3c7a6.chunk.css"
+  },
+  {
+    "revision": "48f8cb84741b1c831777",
+    "url": "./static/js/2.35a6346a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.35a6346a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12c014193c5cd6e99a9",
+    "url": "./static/js/main.5f554db6.chunk.js"
+  },
+  {
+    "revision": "821e3e9e8623f25acdce",
+    "url": "./static/js/runtime-main.62724d6a.js"
+  }
+]);
